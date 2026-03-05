fc2d30f1b4bdbc0b2a6441d39d432d718e18518f
Umirtech <44390455+umirtech@users.noreply.github.com>
2026-03-05 10:59:19 +0530

    Change FFMPEG build configuration from shared to static

--- android.sh
+++ android.sh
@@ -21,7 +21,7 @@ ENABLED_CONFIG="\
 		--enable-decoder=h264,hevc,vp8,vp9,libdav1d,flv,vp6f,adpcm_swf,mpeg4,wmv3,mpeg1video,mpeg2video,mpeg2audio,msmpeg4v2,msmpeg4v3,theora,amrnb,amrwb,dvvideo,h263,mjpeg,png,jpeg,bmp,webp,mp3,aac,ac3,eac3,flac,opus,vorbis,pcm_s16le,pcm_s24le,alac,wma,ass,ssa,mov_text,subrip,webvtt,dvbsub,dvdsub,pgssub \
 		--enable-parser=* \
   		--enable-bsf=* \
-		--enable-shared "
+		--enable-static "
 
 
 ### Disable FFMPEG BUILD MODULES ####
@@ -35,7 +35,7 @@ DISABLED_CONFIG="\
 		--disable-libxml2 \
 		--disable-avdevice \
 		--disable-network \
-		--disable-static \
+		--disable-shared \
 		--disable-debug \
 		--disable-ffplay \
   		--disable-ffprobe \
