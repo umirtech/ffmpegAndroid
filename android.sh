5dbc6d80a32b63444f697f7ee52742e369830de1
Umirtech <44390455+umirtech@users.noreply.github.com>
2026-03-09 17:21:11 +0530

    Add utvideo encoder to android.sh

--- android.sh
+++ android.sh
@@ -17,8 +17,8 @@ ENABLED_CONFIG="\
 		--enable-libdav1d \
 		--enable-muxer=wav,mp4,matroska,webm,mp3,adts,flac,ogg,opus,mpegts,hls,dash,segment \
     	--enable-demuxer=* \
-	 	--enable-encoder=mjpeg \
-		--enable-decoder=h264,hevc,vp8,vp9,libdav1d,flv,vp6f,adpcm_swf,mpeg4,wmv3,mpeg1video,mpeg2video,mpeg2audio,msmpeg4v2,msmpeg4v3,theora,amrnb,amrwb,dvvideo,h263,mjpeg,png,jpeg,bmp,webp,mp3,aac,ac3,eac3,flac,opus,vorbis,pcm_s16le,pcm_s24le,alac,wma,ass,ssa,mov_text,subrip,webvtt,dvbsub,dvdsub,pgssub \
+	 	--enable-encoder=mjpeg,utvideo \
+		--enable-decoder=h264,hevc,vp8,vp9,libdav1d,flv,vp6f,adpcm_swf,mpeg4,wmv3,mpeg1video,mpeg2video,mpeg2audio,msmpeg4v2,msmpeg4v3,theora,amrnb,amrwb,dvvideo,h263,mjpeg,png,jpeg,bmp,webp,mp3,aac,ac3,eac3,flac,opus,vorbis,pcm_s16le,pcm_s24le,alac,wma,ass,ssa,mov_text,subrip,webvtt,dvbsub,dvdsub,pgssub,utvideo \
 		--enable-parser=* \
   		--enable-bsf=* \
 		--enable-static "
