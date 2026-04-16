79f962b0e60e27634fd6ac7170975e9becbdb820
Umirtech <44390455+umirtech@users.noreply.github.com>
2026-04-16 11:00:47 +0530

    Update output path for libffmpeg.so

--- android.sh
+++ android.sh
@@ -173,7 +173,7 @@ configure_ffmpeg(){
    make -j2
    make install -j2
 
-   ${CLANG} -shared -o libffmpeg.so \
+   ${CLANG} -shared -o ${PREFIX}/lib/libffmpeg.so \
   	$(find libavcodec libavformat libavutil libswresample libswscale -name "*.o") \
   	-lm -lz -ldl
    
