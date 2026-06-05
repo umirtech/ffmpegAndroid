7ce873843645eb5ffcb12cef594963fa0a30c76b
Umirtech <44390455+umirtech@users.noreply.github.com>
2026-06-05 15:03:32 +0530

    Refactor decoder options for better readability

--- android.sh
+++ android.sh
@@ -18,7 +18,14 @@ ENABLED_CONFIG="\
 		--enable-muxer=wav,mp4,matroska,webm,mp3,adts,flac,ogg,opus,mpegts,hls,dash,segment \
     	--enable-demuxer=* \
 	 	--enable-encoder=mjpeg,utvideo \
-		--enable-decoder=h264,hevc,vp8,vp9,libdav1d,flv,vp6f,adpcm_swf,mpeg4,wmv3,mpeg1video,mpeg2video,mpeg2audio,msmpeg4v2,msmpeg4v3,theora,truehd,amrnb,amrwb,dvvideo,h263,mjpeg,png,jpeg,bmp,webp,mp3,aac,ac3,eac3,flac,opus,vorbis,pcm_s16le,pcm_s24le,alac,wma,ass,ssa,mov_text,subrip,webvtt,dvbsub,dvdsub,pgssub,utvideo \
+		--enable-decoder=\
+			h264,hevc,vp8,vp9,libdav1d,flv,vp6f,gif,\
+			adpcm_swf,mpeg4,wmv3,mpeg1video,mpeg2video,\
+			mpeg2audio,msmpeg4v2,msmpeg4v3,theora,truehd,\
+			amrnb,amrwb,dvvideo,h263,mjpeg,png,jpeg,bmp,\
+			webp,mp3,aac,ac3,eac3,flac,opus,vorbis,pcm_s16le,\
+			pcm_s24le,alac,wma,ass,ssa,mov_text,subrip,webvtt,\
+			dvbsub,dvdsub,pgssub,utvideo \
 		--enable-parser=* \
 		--enable-bsf=* \
 		--enable-protocol=file \
